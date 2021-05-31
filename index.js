418a1f69acd2a178b87504a229cd975e31b9647b
ecleaver <37559968+ecleaver@users.noreply.github.com>
2021-05-31 09:01:04 -0400

    completed binary tree search

--- index.js
+++ index.js
@@ -1,8 +1,40 @@
-// Import stylesheets
-import './style.css';
 
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
-alert("Hellow world");
+
+numbers = [45, 75, 12, 13, 14, 16, 78, 55, 20]
+
+const binaryTreeSearch = (arr, number) => {
+
+    arr.sort();
+
+
+    const binary = (arr, number) => {
+
+        middle = Math.ceil(arr.length/2) 
+        if(number == arr[middle]) {
+            // console.log("Testing middle")
+            return number;
+        }
+        else if(arr.length == 1) {
+            if(arr[0] == number) {
+                return number;
+            } 
+        } else if(number > arr[middle]) {
+            arr = arr.splice(middle + 1);
+            return binary(arr, number);
+        } else if(number < arr[middle]) {
+            arr = arr.splice(0, middle);
+            return binary(arr, number);
+        } else {
+            return `Number ${number} was not found.`
+        }
+
+    }
+
+    return binary(arr, number);
+}
+
+console.log(`Test for the middle: ${binaryTreeSearch(numbers, 45)}`)
+console.log(`Test for the right: ${binaryTreeSearch(numbers, 55)}`)
+console.log(`Test for the left: ${binaryTreeSearch(numbers, 12)}`)
+console.log(`Test for not found: ${binaryTreeSearch(numbers, 22)}`)
