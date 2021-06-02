574ecd59e4e51e6a2720d069b599822da3a5874d
ErinCleaver-GRCC <erincleaver@email.grcc.edu>
2021-06-01 20:33:40 -0400

    added commits

--- index.js
+++ index.js
@@ -1,27 +1,34 @@
 numbers = [45, 75, 12, 13, 14, 16, 78, 55, 20]
 
 const numberBinaryTreeSearch = (numbers, number) => {
-
+    // sorts the numbers within the function
     numbers.sort();
 
-
+    // function for finding numbers in a binary.
     const binary = (numbers, number) => {
 
+        // finds the middle
         middle = Math.ceil(numbers.length/2) 
+
+        // checks if the current number is equal to the middle
         if(number == numbers[middle]) {
             // console.log("Testing middle")
             return `Number ${number} was found.`;
         }
+        // checks if the length is equal to 1
         else if(numbers.length == 1) {
             if(numbers[0] == number) {
                 return `Number ${number} was found.`;
             } 
+        // checks if the current point is greate then the number.
         } else if(number > numbers[middle]) {
             numbers = numbers.splice(middle + 1);
             return binary(numbers, number);
+        // checks if the current number in the array is less then the number.
         } else if(number < numbers[middle]) {
             numbers = numbers.splice(0, middle);
             return binary(numbers, number);
+        // if it dose not return true it will print that the number is not found.
         } else {
             return `Number ${number} was not found.`
         }
